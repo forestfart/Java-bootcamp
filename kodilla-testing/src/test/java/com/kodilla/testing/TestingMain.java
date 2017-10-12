ae07e5013186ce4207ac5e7e59c68877b94ddb88
Michal Michalik <michal.michalik@gmail.com>
2017-10-12 20:48:58 +0200

    troubles solved

--- kodilla-testing/src/test/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,5 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
     public static void main(String[] args){
     }
