802c9f71838b8bd2e96c64813140c4df36e8205a
Michal Michalik <michal.michalik@gmail.com>
2018-03-07 22:29:44 +0100

    tests for rps generator improved, git ignore now in place

--- kodilla-rps/src/test/java/com/kodilla/rps/ShapeGeneratorTest.java
+++ kodilla-rps/src/test/java/com/kodilla/rps/ShapeGeneratorTest.java
@@ -1,44 +1,86 @@
 package com.kodilla.rps;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static com.kodilla.rps.Shapes.*;
+
 public class ShapeGeneratorTest {
-    String userShape, generatedShape;
+    Shapes userShape, generatedShape;
     ShapeGenerator shapeGenerator;
+    int paperCount, scissorsCount, stoneCount;
 
     @Before
     public void setUp() {
+        paperCount = scissorsCount = stoneCount = 0;
         shapeGenerator = new ShapeGenerator();
     }
 
     @Test
     public void testShapeGeneratorWithStone() {
+        // Given
+        userShape = STONE;
+
         // When
-        userShape = "stone";
-        generatedShape = shapeGenerator.generateShape(userShape);
+        for (int i = 0; i < 100000; i++)
+            switch (shapeGenerator.generateShape(userShape)) {
+                case PAPER:
+                    paperCount++;
+                    break;
+                case STONE:
+                    stoneCount++;
+                    break;
+                case SCISSORS:
+                    scissorsCount++;
+            }
 
         // Then
-        Assert.assertTrue(generatedShape.length() > 4);
+        Assert.assertThat(Math.abs(paperCount-(stoneCount+scissorsCount)), Matchers.lessThan(1000));
     }
 
     @Test
     public void testShapeGeneratorWithPaper() {
+        // Given
+        userShape = PAPER;
+
         // When
-        userShape = "paper";
-        generatedShape = shapeGenerator.generateShape(userShape);
+        for (int i = 0; i < 100000; i++)
+            switch (shapeGenerator.generateShape(userShape)) {
+                case PAPER:
+                    paperCount++;
+                    break;
+                case STONE:
+                    stoneCount++;
+                    break;
+                case SCISSORS:
+                    scissorsCount++;
+            }
 
         // Then
-        Assert.assertTrue(generatedShape.length() > 4);
+        Assert.assertThat(Math.abs((paperCount+stoneCount)-scissorsCount), Matchers.lessThan(20000));
     }
+
     @Test
     public void testShapeGeneratorWithScissors() {
+        // Given
+        userShape = SCISSORS;
+
         // When
-        userShape = "scissors";
-        generatedShape = shapeGenerator.generateShape(userShape);
+        for (int i = 0; i < 100000; i++)
+            switch (shapeGenerator.generateShape(userShape)) {
+                case PAPER:
+                    paperCount++;
+                    break;
+                case STONE:
+                    stoneCount++;
+                    break;
+                case SCISSORS:
+                    scissorsCount++;
+            }
 
         // Then
-        Assert.assertTrue(generatedShape.length() > 4);
+        Assert.assertThat(Math.abs(stoneCount-(paperCount+scissorsCount)), Matchers.lessThan(10000));
     }
 }
